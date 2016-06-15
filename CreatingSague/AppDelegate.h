7d8a5cc20753333ac07a486d0c0cfae62892f189
Prashanth Sirikonda <prashanth@prashanths-macbook-pro.local>
2016-06-15 10:50:05 -0400

    Initial Commit

---
+++ CreatingSague/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CreatingSague
+//
+//  Created by Prashanth Sirikonda on 6/15/16.
+//  Copyright © 2016 prashanthsirikonda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CreatingSague/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CreatingSague
+//
+//  Created by Prashanth Sirikonda on 6/15/16.
+//  Copyright © 2016 prashanthsirikonda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
